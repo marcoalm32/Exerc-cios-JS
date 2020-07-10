62395339eb24a2147fc4fc4a37f9abd0b333a7ac
marcoalm32 <marcoalm25@gmail.com>
2020-07-10 11:43:11 -0300

    exercicio07 JS

---
+++ exercicio07/exe07.js
@@ -0,0 +1,13 @@
+var num = document.getElementsByClassName("numero")
+function subtrair() {
+    num[0].innerHTML --
+}
+
+function somar(){
+    num[0].innerHTML ++
+
+}
+
+function zerar(){
+    num[0].innerHTML = '0'
+}
